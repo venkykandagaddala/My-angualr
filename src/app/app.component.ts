1bbdc8123a8ea86ed02a2abb62962e9487f6f291
venky <=>
2019-09-03 23:44:43 +0530

    Data binding + pipes + interface + life cycle hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import {Component} from "@angular/core"
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "pm-root",
+  template: `
+    <div class="col-md-12"><br>
+      <h1 class="text-center">{{pageTitle}}</h1><br>
+      <pm-products></pm-products>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'APM';
+export class AppComponent{
+  pageTitle: string = "ACME product Management";
 }
