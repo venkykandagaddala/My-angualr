6a01a760828949c0e02d0bb4bee4b1838b28e1de
venky <=>
2019-09-07 00:33:01 +0530

    Navigation and Routing Additional Techniques

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule} from "@angular/router"
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacePipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 
 @NgModule({
@@ -24,12 +24,11 @@ import { WelcomeComponent } from './home/welcome.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       { path: "products", component: ProductListComponent },
-      { path: "products/:id", component: ProductDetailComponent },
+      { path: "products/:id", canActivate: [ProductDetailGuard], component: ProductDetailComponent },
       { path: "welcome", component: WelcomeComponent },
       { path: "", redirectTo: 'welcome', pathMatch: "full" },
       { path: "**", redirectTo: 'welcome', pathMatch: "full"}
