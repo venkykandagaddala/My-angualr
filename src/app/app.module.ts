e50224cbe161ceb02895f07bbc30bd955fce258a
venky <=>
2019-09-06 21:35:30 +0530

    Retrieving data using HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list.component';
 import { ConvertToSpacePipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { StarComponent } from './shared/star.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product-list.component.ts
+++ src/app/product/product-list.component.ts
@@ -11,6 +11,7 @@ export class ProductListComponent implements OnInit{
   imageWidth: number = 30;
   imageMarging: number = 2;
   showImage: boolean = false;
+  errorMessage:  string = "";
   
   _listFilter: string;
   
@@ -40,8 +41,13 @@ export class ProductListComponent implements OnInit{
   }
   ngOnInit(): void{
     console.log("OnInit Method");
-    this.products = this.productService.getProducts();
-    this.filterdProducts = this.products;
+    this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products
+        this.filterdProducts = this.products;},
+      error: err => this.errorMessage = err
+    });
+    
   }
   onRatingClicked(message: string): void{
     this.pageTitle = "Product List  -  " + message; 
