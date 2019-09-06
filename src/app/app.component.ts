7225b43aa49bc1a69d7cab072c21895502ad7fc8
venky <=>
2019-09-06 23:25:24 +0530

    Navigation and Routing Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
 import {Component} from "@angular/core"
 @Component({
   selector: "pm-root",
-  template: `
-    <div class="col-md-12"><br>
-      <h1 class="text-center">{{pageTitle}}</h1><br>
-      <pm-products></pm-products>
-    </div>
-  `
+  templateUrl: "./app.component.html"
 })
 export class AppComponent{
   pageTitle: string = "ACME product Management";
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 @Component({
-  selector: "pm-products",
   templateUrl: "./product-list.component.html",
   styleUrls: ['./product-list.component.css']
 })
